5ddaecb2b117260c40dc8318f5ce36b9c2d56709
sukyoung11 <sukyoung7371@gmail.com>
2022-02-28 02:13:54 +0900

    [BOJ] 아기상어 / 골드3 / 해결

--- Sukyoung/16236_아기 상어.py
+++ Sukyoung/16236_아기 상어.py
@@ -1,8 +1,8 @@
 from collections import deque
+
 def bfs(start_x,start_y,baby):
     global fish
     global eat
-
     queue = deque()
     queue.append([start_x,start_y])
     visited = [[0] * N for _ in range(N)]
@@ -20,11 +20,11 @@ def bfs(start_x,start_y,baby):
 
             if 0 <= nx < N and 0 <= ny < N:
                 if mat[nx][ny] <= baby and visited[nx][ny] == 0:
-                    queue.append([nx, ny])  # 방문 예정 리스트 추가
+                    queue.append([nx, ny])
                     visited[nx][ny] = 1  # 방문처리
                     count[nx][ny] = count[x][y] + 1  # 시작점으로 부터 이동 거리 카운트
-                    if mat[nx][ny] < baby and mat[nx][ny] != 0: # 도착하면, 카운트한 이동 거리 반환
-                        eat.append([nx,ny,count[nx][ny]])
+                    if mat[nx][ny] < baby and mat[nx][ny] != 0:
+                        eat.append([nx,ny,count[nx][ny]]) # 먹을 수 있는 물고기 위치, 거리 모두 리스트에 추가
 
     return eat
 
@@ -36,11 +36,10 @@ dx = [-1, 0, 0, 1]
 dy = [0, -1, 1, 0]
 
 result = 0
-baby = 2
-fish = 0
-fishlist = []
+baby = 2  # 현재 상어 크기
+fish = 0  # 지금까지 먹은 물고기 수
 
-def find(mat):
+def find(mat):  # 상어 현재 위치 찾는 함수
     global row
     global col
     for row in range(N):
@@ -51,18 +50,16 @@ def find(mat):
 while 1:
     find(mat)
     bfs(row,col,baby)
-    min_eat = sorted(eat,key=lambda x:(x[2],x[0],x[1]))
-    if eat:
-        mat[min_eat[0][0]][min_eat[0][1]] = 9
-        mat[row][col] = 0
-        fish += 1
-        result += min_eat[0][2]
-        if baby == fish:
+    min_eat = sorted(eat,key=lambda x:(x[2],x[0],x[1])) # 가까운순, 가장 위쪽, 가장 왼쪽 순서로 정렬
+    if eat:  # 먹을 수 있는 물고기가 있으면
+        mat[min_eat[0][0]][min_eat[0][1]] = 9  # 정렬 결과 중 가장 앞의 물고기 먹고 이동
+        mat[row][col] = 0  # 원래 상어가 있던 자리는 0
+        fish += 1  # 지금까지 먹은 물고기 +1
+        result += min_eat[0][2]  # 지금까지 이동한 거리 (=시간)
+        if baby == fish:  # 자기 크기만큼 먹으면, 자기 크기 +1
             baby+=1
             fish = 0
-    else:
+    else:  # 먹을 수 있는 물고기 없으면 끝
         break
-
-fishlist =[]
 
 print(result)
