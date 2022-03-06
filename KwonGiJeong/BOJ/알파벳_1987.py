3f835e4c5c65f2f8246f8c3b6b581899f10fa828
gijeogiya <msg012525@gmail.com>
2022-03-06 13:35:23 +0900

    알파벳/1987/골드4/시간초과/30분

---
+++ KwonGiJeong/BOJ/알파벳_1987.py
@@ -0,0 +1,35 @@
+import sys
+
+sys.stdin = open('input_1987.txt')
+
+
+def DFS(row, col, cnt):
+
+    global alphabets
+    global result
+
+    if cnt > result:
+        result = cnt
+
+    d_row = [0, 0, 1, -1]
+    d_col = [1, -1, 0, 0]
+
+    for i in range(4):
+        n_row = row + d_row[i]
+        n_col = col + d_col[i]
+
+        if 0 <= n_row < len(matrix) and 0 <= n_col < len(matrix[0]) and not matrix[n_row][n_col] in alphabets:
+            alphabets.add(matrix[n_row][n_col])
+            DFS(n_row, n_col, cnt+1)
+            alphabets.remove(matrix[n_row][n_col])
+
+R, C = map(int, input().split())
+matrix = []
+for _ in range(R):
+    matrix.append(list(input()))
+
+result = 0
+alphabets = set(matrix[0][0])
+
+DFS(0, 0, 1)
+print(result)
