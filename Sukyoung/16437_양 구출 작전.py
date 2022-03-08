9796118686f67b30628499e46bdf68cbe8b8bd87
sukyoung11 <sukyoung7371@gmail.com>
2022-03-09 00:24:37 +0900

    [BOJ]양 구출 작전 / 골드2 / 시간초과

--- Sukyoung/16437_양 구출 작전.py
+++ Sukyoung/16437_양 구출 작전.py
@@ -1,27 +1,45 @@
+
 def dfs(start_list):
     global result
 
+
+
+
     for i in start_list:
         result.append(i[1])
-        print(result)
 
         if dict[i[0]]:
             dfs(dict[i[0]])
             result.pop()
 
-        else:
-            result_list.append(result)
+        elif not dict[i[0]]:
+            sum = 0
+            for i in range(len(result)-1,-1,-1):
+                if result[i] > 0:
+                    k = i
+                    break
+            for j in range(k,-1,-1):
+                sum += result[j]
+                num = sum - result[j]
+
+                if sum < 0:
+                    sum = 0
+                if result[j] < 0:
+                    result[j] += num
+                    if result[j] > 0:
+                        result[j] = 0
+                elif result[j] > 0:
+                    result[j] = 0
+            sum_list.append(sum)
             result.pop()
-            print('k')
-            print(result_list)
 
 
 N = int(input())
-information = []
+information = [[1,'S',0,0]]
 for i in range(2,N+1):
     island = input().split()
     information.append([i,island[0],island[1],island[2]])
-print(information)
+
 for info in information:
     if info[1] == 'S':
         info[2] = int(info[2])
@@ -29,17 +47,19 @@ for info in information:
         info[2] = -1 * int(info[2])
 
 dict = {}
-for i in range(1,N+1):
+for i in range(N+1):
     list_ = []
     for info in information:
         if i == int(info[3]):
             list_.append([info[0],info[2]])
     dict[i] = list_
-print(dict)
+
+visited = [False]*(N+1)
+
 result =[]
-result_list=[]
-dfs(dict[1])
+sum_list=[]
+dfs(dict[0])
 # answer=[i for i in dfs(dict[1]) if i > 0]
-print(result_list)
+print(sum(sum_list))
 #print(sum(answer))
 
--- Sukyoung/test.py
+++
@@ -1,3 +0,0 @@
-a= [1,2,3,4]
-a.remove(2)
-print(a)
