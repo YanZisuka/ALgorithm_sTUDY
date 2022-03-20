371cc9636f1bbc355eabc256077111dd4ab325aa
YanZisuka <hanndrednine@gmail.com>
2022-03-20 12:59:18 +0900

    [BOJ] 동전 1 / 골드5 / 20분

---
+++ HanSeungJae/동전1_2293.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+dp = [0] * (k+1)
+
+for i in range(n):
+    coin = int(input())
+
+    for j in range(k+1):
+        if j == coin:
+            dp[j] += 1
+        else:
+            if j - coin >= 0:
+                dp[j] = dp[j] + dp[j-coin]
+
+print(dp)
