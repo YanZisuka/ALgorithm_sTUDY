decd1db703b737f07ddf271583f8f74c642d1cba
sukyoung11 <sukyoung7371@gmail.com>
2022-03-23 14:50:40 +0900

    [BOJ] 이동하기 / 실버1 / 10m

--- Sukyoung/2193_이친수.py
+++ Sukyoung/2193_이친수.py
@@ -1,8 +1,8 @@
-count = [1,1]
-for i in range(3,91):
-    count.append(count[i-2]+count[i-3])
+count = [1, 1]
+for i in range(3, 91):
+    count.append(count[i - 2] + count[i - 3])
 N = int(input())
-print(count[N-1])
+print(count[N - 1])
 
 # N = 91 까지 리스트를 만들어 놓고 나중에 값을 입력받으면 통과
 # 입력을 먼저 받고, 그 수까지만 리스트 만들면 런타임에러 (왜 시간초과가 아니고 런타임 에러 뜰까?)
