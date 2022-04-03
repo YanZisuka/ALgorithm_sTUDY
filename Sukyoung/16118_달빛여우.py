a2cde7ba044fe0bbbad62a646f9af89f7d123697
sukyoung11 <sukyoung7371@gmail.com>
2022-04-03 13:46:18 +0900

    [BOJ] 달빛여우 / 골드1 / 하는중

--- Sukyoung/16118_달빛여우.py
+++ Sukyoung/16118_달빛여우.py
@@ -2,27 +2,27 @@ import heapq
 import sys
 input = sys.stdin.readline
 
-def fox(start):
+def fox(start): # 여우의 최단 거리 - 기존 다익스트라
     queue = []
-    heapq.heappush(queue,[0,start])  # heap에 담아서 최단 거리를 빨리 찾게 함
+    heapq.heappush(queue,[0,start])
     final_distance[start] = 0
 
     while queue:
         distance,node = heapq.heappop(queue)
-        if distance <= final_distance[node]:  # 탐색 한 적 없는 경로라면
-            for i in info[node]:  # 탐색
-                sum_distance = i[1]+distance  # 해당 위치까지의 거리 + 가중치
-                if sum_distance < final_distance[i[0]]:  # 기존에 구한 거리보다 짧으면
-                    final_distance[i[0]] = sum_distance  # 갱신
-                    heapq.heappush(queue,[sum_distance,i[0]])  # queue에 삽입
+        if distance <= final_distance[node]:
+            for i in info[node]:
+                sum_distance = i[1]+distance
+                if sum_distance < final_distance[i[0]]:
+                    final_distance[i[0]] = sum_distance
+                    heapq.heappush(queue,[sum_distance,i[0]])
 
 V, E = map(int,input().split())
 info = [[] for _ in range(V+1)]
-final_distance = [float('inf')]*(V+1)  # 초기값은 무한대로 설정
+final_distance = [float('inf')]*(V+1)
 
 for i in range(E):
     u,v,w = map(int,input().split())
-    info[u].append([v,w])  # [연결노드 , 가중치] 정보 삽입
+    info[u].append([v,w])
 
 fox(1)
 print(info)
