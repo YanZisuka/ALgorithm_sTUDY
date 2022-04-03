fed8c58692709c8dc02279349042bb04fa8390e6
YanZisuka <hanndrednine@gmail.com>
2022-04-03 10:45:33 +0900

    [BOJ] 달빛 여우 / 골드1 / 1시간20분

---
+++ HanSeungJae/달빛여우_16118.py
@@ -0,0 +1,70 @@
+from heapq import *
+import sys
+input = sys.stdin.readline
+
+
+def foxDijkstra(s):
+    dist = [float('inf')] * (n+1)
+    dist[s] = 0
+    heap = []
+    heappush(heap, (0, s))
+
+    while heap:
+        dt, now = heappop(heap)
+
+        if dist[now] < dt:
+            continue
+
+        for next, weight in graph[now]:
+            shortest = dist[now] + weight
+            if shortest < dist[next]:
+                dist[next] = shortest
+                heappush(heap, (shortest, next))
+    return dist
+
+
+def wolfDijkstra(s):
+    dist = [[float('inf')] * (n+1) for _ in range(2)]
+    dist[1][s] = 0
+    heap = []
+    heappush(heap, (0, s, False))
+
+    while heap:
+        dt, now, speedy = heappop(heap)
+
+        if speedy and dist[0][now] < dt:
+            continue
+        if not speedy and dist[1][now] < dt:
+            continue
+        
+        for next, weight in graph[now]:
+            if speedy:
+                shortest = dt + weight * 2
+                if shortest < dist[1][next]:
+                    dist[1][next] = shortest
+                    heappush(heap, (shortest, next, False))
+            if not speedy:
+                shortest = dt + weight / 2
+                if shortest < dist[0][next]:
+                    dist[0][next] = shortest
+                    heappush(heap, (shortest, next, True))
+    return dist
+
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+ans = 0
+
+for i in range(m):
+    a, b, d = map(int, input().split())
+    graph[a].append((b, d))
+    graph[b].append((a, d))
+
+fox = foxDijkstra(1)
+wolf = wolfDijkstra(1)
+
+for i in range(1, n+1):
+    if fox[i] < min(wolf[0][i], wolf[1][i]):
+        ans += 1
+
+print(ans)
