90618659aff1d348186ae9c128697ccecd9ad097
YanZisuka <hanndrednine@gmail.com>
2022-04-10 10:29:18 +0900

    [BOJ] 게임 개발 / 골드3 / 1시간

---
+++ HanSeungJae/게임개발_1516.py
@@ -0,0 +1,45 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+
+def topologySort():
+    result = [0] * (n+1)
+    q = deque()
+
+    for i in range(1, n+1):
+        if inDegree[i] == 0:
+            q.append(i)
+
+    while q:
+        now = q.popleft()
+        result[now] += cost[now]
+
+        for nxt in graph[now]:
+            inDegree[nxt] -= 1
+            result[nxt] = max(result[nxt], result[now])
+            if inDegree[nxt] == 0:
+                q.append(nxt)
+                
+    return result
+
+
+n = int(input())
+graph = [[] for _ in range(n+1)]
+inDegree = [0] * (n+1)
+cost = [0] * (n+1)
+
+for i in range(1, n+1):
+    info = list(map(int, input().split()))
+    cost[i] = info[0]
+
+    j = 1
+    while info[j] != -1:
+        graph[info[j]].append(i)
+        inDegree[i] += 1
+        j += 1
+
+answer = topologySort()
+
+for i in range(1, n+1):
+    print(answer[i])
