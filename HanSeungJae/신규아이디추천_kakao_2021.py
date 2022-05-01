d6b5da2c316bfeeb891219a45aa003b5111aa3ea
YanZisuka <hanndrednine@gmail.com>
2022-05-01 11:39:50 +0900

    [PGS] 신규 아이디 추천 / Lv. 1 / 10분

--- HanSeungJae/신규아이디추천_kakao_2021.py
+++ HanSeungJae/신규아이디추천_kakao_2021.py
@@ -1,3 +1,4 @@
+"""
 from collections import deque
 
 def solution(new_id):
@@ -36,3 +37,20 @@ def solution(new_id):
         stack.pop()
         
     return ''.join(map(str, stack))
+"""
+
+import re
+
+def solution(new_id):
+    new_id = new_id.lower()
+    new_id = re.sub('[^a-z0-9-_.]', '', new_id)
+    while '..' in new_id:
+        new_id = new_id.replace('..', '.')
+    new_id = new_id[1:] if new_id and new_id[0] == '.' else new_id
+    new_id = new_id[:-1] if new_id and new_id[-1] == '.' else new_id
+    new_id = 'a' if not new_id else new_id
+    new_id = new_id[:15] if len(new_id) >= 16 else new_id
+    new_id = new_id[:-1] if new_id and new_id[-1] == '.' else new_id
+    while len(new_id) <= 2:
+        new_id += new_id[-1]
+    return new_id
