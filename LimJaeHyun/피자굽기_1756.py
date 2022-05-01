3cdbdb31736f88ec3d9d58c2fb2841c377185b98
relajar1 <relajar@naver.com>
2022-05-01 10:15:33 +0900

    [BOJ] / 피자굽기 / 골드5 / 실패

--- LimJaeHyun/피자굽기_1756.py
+++ LimJaeHyun/피자굽기_1756.py
@@ -3,28 +3,28 @@ oven_width = list(map(int, input().split()))
 dows = list(map(int, input().split()))
 
 
-# pizza_location = [0] * depth
-# for dow_idx, dow in enumerate(dows):
-#     for oven_idx, oven in enumerate(oven_width):
-#         if pizza_location[oven_idx] != 0:
-#             try:
-#                 pizza_location[oven_idx - 1] = dow_idx + 1
-#             except IndexError:
-#                 break
-#
-#         if dow > oven:
-#             try:
-#                 pizza_location[oven_idx - 1] = dow_idx + 1
-#                 oven_width = oven_width[0:oven_idx+1]
-#                 break
-#             except IndexError:
-#                 break
-#         else:
-#             continue
-#
-# if len(set(pizza_location)) < dow_num + 1:
-#     print(0)
-# if len(set(pizza_location)) == dow_num + 1:
-#     print(pizza_location.index(dow_num) + 1)
+pizza_location = [0] * depth
+for dow_idx, dow in enumerate(dows):
+    for oven_idx, oven in enumerate(oven_width):
+        if pizza_location[oven_idx] != 0:
+            try:
+                pizza_location[oven_idx - 1] = dow_idx + 1
+            except IndexError:
+                break
+
+        if dow > oven:
+            try:
+                pizza_location[oven_idx - 1] = dow_idx + 1
+                oven_width = oven_width[0:oven_idx+1]
+                break
+            except IndexError:
+                break
+        else:
+            continue
+
+if len(set(pizza_location)) < dow_num + 1:
+    print(0)
+if len(set(pizza_location)) == dow_num + 1:
+    print(pizza_location.index(dow_num) + 1)
 
 
