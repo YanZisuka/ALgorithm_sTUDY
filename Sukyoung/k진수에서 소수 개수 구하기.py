3c70e5e3abd77e2f78580fe0edeb461cf1bd7847
sukyoung11 <sukyoung7371@gmail.com>
2022-05-03 04:21:43 +0900

    [PGS] K진수에서 소수 개수 구하기 / Lv2 / 10m

--- Sukyoung/k진수에서 소수 개수 구하기.py
+++ Sukyoung/k진수에서 소수 개수 구하기.py
@@ -1,8 +1,8 @@
 def solution(n, k):
 
     def is_prime(x):
-        for i in range(2,int(x**(0.5))+1):
-                if x%i == 0:
+        for i in range(2,int(x**0.5)+1):
+                if x % i == 0:
                     return False
         return True
 
@@ -21,6 +21,6 @@ def solution(n, k):
     for num in result:
         if num and num != '1':
             if is_prime(int(num)):
-                answer+=1
+                answer += 1
 
     return answer
