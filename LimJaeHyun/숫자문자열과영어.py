3bf7b56219cc4cbb3e4454d05a69e9a1e0c9e677
relajar1 <relajar@naver.com>
2022-05-16 23:43:10 +0900

    [PGS] / 숫자문자열과영어 / LV.1 / 15분

---
+++ LimJaeHyun/숫자문자열과영어.py
@@ -0,0 +1,23 @@
+def solution(s):
+    answer = ''
+    numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    string = list(s)
+    print(string)
+    temp = ''
+    for character in string:
+        if character.isdigit():
+            answer += character
+        elif character.isalpha():
+            temp += character
+            if temp in numbers:
+                answer += str(numbers.index(temp))
+                temp = ''
+    answer = int(answer)
+    print(answer)
+    return answer
+
+
+solution('one4seveneight')
+solution('23four5six7')
+solution('2three45sixseven')
+solution('123')
