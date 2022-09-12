a912d48115c662b494b8fde8696322c09151d7fa
YanZisuka <hanndrednine@gmail.com>
2022-09-12 21:32:44 +0900

    [PGS] k진수에서 소수 개수 구하기 / Lv.2 / 10분

--- HanSeungJae/k진수에서소수개수구하기_kakao_2022.py
+++ HanSeungJae/k진수에서소수개수구하기_kakao_2022.py
@@ -1,45 +1,34 @@
 def solution(n, k):
-    n = int(n)
-    k = int(k)
-    
-    def getK(n):
-        prime_num = ''
+    def conv(n, k):
+        res = ''
         while n // k != 0:
-            prime_num = str(n % k) + prime_num
+            res = str((n % k)) + res
             n = n // k
-        prime_num = str(n % k) + prime_num
-        
-        return int(prime_num)
-    
-    def check(num):
-        if num:
-            num = int(num)
-            max_val = int(num**0.5)
-            
-            if num == 1:
-                return False
+        res = str((n % k )) + res
+        return res
 
-            for i in range(2, max_val+1):
-                if num % i == 0:
-                    return False
-                
-            return True
-    
-    ans = 0
-    
-    n = str(getK(n))
-    idx = 0
-    
-    for i in range(len(n)):
-        if n[i] != '0' and i == len(n) - 1:
-            num = n[idx:]
-            if check(num):
-                ans += 1
-                
-        if n[i] == '0':
-            num = n[idx:i]
-            if check(num):
-                ans += 1
-            idx = i+1
-    
-    return ans
+    def check(num):
+        if num == 1: return False
+        n = 2
+        while n <= (num) ** 0.5:
+            if num % n == 0: return False
+            n += 1
+        return True
+
+    answer = 0
+    res = conv(n, k)
+    cand = res.split('0')
+    for c in cand:
+        if not c: continue
+        target = int(c)
+        if check(target):
+            answer += 1
+            
+    return answer
+
+
+
+
+
+print(solution(437674, 3))
+print(solution(110011, 10))
