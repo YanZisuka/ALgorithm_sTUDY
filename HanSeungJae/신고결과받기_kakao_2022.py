905fc01f80ee70249301fd98d07e29d23716baab
YanZisuka <hanndrednine@gmail.com>
2022-09-13 23:58:14 +0900

    [PGS] 신고 결과 받기 / Lv. 1 / 10분

--- HanSeungJae/신고결과받기_kakao_2022.py
+++ HanSeungJae/신고결과받기_kakao_2022.py
@@ -1,34 +1,18 @@
 def solution(id_list, report, k):
-    n = len(id_list)
-    answer = []
-    id_mat = [[0, [], 0, 0] for _ in range(n)]
-    report = set(report)
-    id_dict = {}
-    
-    for i in range(n):
-        id_mat[i][0] = id_list[i]
-        id_dict.setdefault(id_list[i], i)
-        
-    while report:
-        ff, tt = report.pop().split(' ')
-        id_mat[id_dict.get(ff)][1].append(tt)
-        id_mat[id_dict.get(tt)][2] += 1  # 신고받은 횟수
-        
-    # for i in range(m):
-    #     ff, tt = report[i].split(' ')
-    #     for j in range(n):
-    #         if id_mat[j][0] == ff:
-    #             id_mat[j][1].append(tt)
-    #         elif id_mat[j][0] == tt:
-    #             id_mat[j][2] += 1  # 신고받은 횟수
-                
-    for i in range(n):
-        if id_mat[i][2] >= k:
-            for j in range(n):
-                if id_mat[i][0] in id_mat[j][1]:
-                    id_mat[j][3] += 1  # 메일받는 횟수
-                    
-    for i in range(n):
-        answer.append(id_mat[i][3])
-        
+    answer = [0] * len(id_list)
+    table = {id: set() for id in id_list}
+    for r in report:
+        ff, tt = r.split()
+        table[tt].add(ff)
+
+    for v in table.values():
+        if len(v) >= k:
+            for ff in v:
+                answer[id_list.index(ff)] += 1
+
     return answer
+
+
+
+
+print(solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2))
