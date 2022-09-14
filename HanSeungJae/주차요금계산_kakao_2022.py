e4f33880cc3c66d2ccc47aad434cbae2af9388ef
YanZisuka <hanndrednine@gmail.com>
2022-09-14 23:53:53 +0900

    [PGS] 주차 요금 계산 / Lv. 2 / 10분

--- HanSeungJae/주차요금계산_kakao_2022.py
+++ HanSeungJae/주차요금계산_kakao_2022.py
@@ -2,40 +2,40 @@ import math
 
 def solution(fees, records):
     answer = []
-    tmp = []
+    preprocess = []
     parkings = {}
     times = {}
     
     for i in range(len(records)):
-        time, carNum, status = records[i].split()
+        time, car, status = records[i].split()
         hh, mm = map(int, time.split(':'))
         time = hh * 60 + mm
         
         if status == 'IN':
-            parkings[carNum] = time
-            if not times.get(carNum):
-                times[carNum] = 0
+            parkings[car] = time
+            if not times.get(car):
+                times[car] = 0
         elif status == 'OUT':
-            times[carNum] += time - parkings[carNum]
-            parkings[carNum] = 'c'
+            times[car] += time - parkings[car]
+            parkings[car] = 'done'
     
     for i in range(len(records)):
-        time, carNum, status = records[i].split()
-        if parkings[carNum] != 'c':
+        time, car, status = records[i].split()
+        if parkings[car] != 'done':
             time = 23 * 60 + 59
-            times[carNum] += time - parkings[carNum]
-            parkings[carNum] = 'c'
+            times[car] += time - parkings[car]
+            parkings[car] = 'done'
     
     for key, value in times.items():
         fee = fees[1]
         if value > fees[0]:
             value = math.ceil((value - fees[0]) / fees[2])
             fee += value * fees[3]
-        tmp.append([key, fee])
+        preprocess.append([key, fee])
     
-    tmp.sort()
+    preprocess.sort()
     
-    for feeInfo in tmp:
-        answer.append(feeInfo[1])
+    for fee_info in preprocess:
+        answer.append(fee_info[1])
         
     return answer
