f6658158e8bb17d473565d7ca43ac27088e55123
YanZisuka <hanndrednine@gmail.com>
2022-09-23 21:13:22 +0900

    [PGS] 파괴되지 않은 건물 / Lv. 3 / 2시간

--- HanSeungJae/파괴되지않은건물_kakao_2022.py
+++ HanSeungJae/파괴되지않은건물_kakao_2022.py
@@ -1,4 +1,7 @@
+from pprint import pprint
+
 def solution(board, skill):
+    m, n = len(board), len(board[0])
     answer = 0
     tmp = [[0] * (len(board[0]) + 1) for _ in range(len(board) + 1)]
 
@@ -8,16 +11,23 @@ def solution(board, skill):
         tmp[r2 + 1][c1] += -deg if type == 2 else deg
         tmp[r2 + 1][c2 + 1] += deg if type == 2 else -deg
 
-    for i in range(len(tmp) - 1):
-        for j in range(len(tmp[0]) - 1):
+    for i in range(m):
+        for j in range(n):
             tmp[i][j + 1] += tmp[i][j]
 
-    for j in range(len(tmp[0]) - 1):
-        for i in range(len(tmp) - 1):
+    for j in range(n):
+        for i in range(m):
             tmp[i + 1][j] += tmp[i][j]
 
-    for i in range(len(board)):
-        for j in range(len(board[i])):
+    for i in range(m):
+        for j in range(n):
             board[i][j] += tmp[i][j]
             if board[i][j] > 0: answer += 1
     return answer
+
+
+
+
+
+print(solution([[5,5,5,5,5],[5,5,5,5,5],[5,5,5,5,5],[5,5,5,5,5]],[[1,0,0,3,4,4],[1,2,0,2,3,2],[2,1,0,3,1,2],[1,0,1,3,3,1]])) # 10
+print(solution([[1,2,3],[4,5,6],[7,8,9]],[[1,1,1,2,2,4],[1,0,0,1,1,2],[2,2,0,2,0,100]])) # 6
